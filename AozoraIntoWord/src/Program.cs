627c2f4228378a4981b158ba0d745aee92e3edee
tnagao7 <tnagao@wgag.net>
2017-08-20 00:24:09 +0900

    Update copyright notice

--- AozoraIntoWord/src/Program.cs
+++ AozoraIntoWord/src/Program.cs
@@ -1,7 +1,4 @@
-﻿// Hoge Text Replacer
-// Software License Agreement (MIT License)
-//
-// Copyright (c) 2013 Takahiro Nagao
+﻿// Copyright (c) 2013 Takahiro Nagao
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
