a1ace3a15ff6d95a9e96567930a375adb8f14085
tnagao7 <tnagao@wgag.net>
2017-08-20 19:56:28 +0900

    Update solution

--- AozoraIntoWord/Properties/AssemblyInfo.cs
+++ AozoraIntoWord/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("青空-Word 変換")]
+[assembly: AssemblyTitle("青空→Word 変換")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("青空-Word 変換")]
-[assembly: AssemblyCopyright("Copyright (c) 2013 tnagao7")]
+[assembly: AssemblyProduct("青空→Word 変換")]
+[assembly: AssemblyCopyright("Copyright (c) 2013-2017 tnagao7")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.1")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
